b726f2655a73e4cc171b6e8b8362c0bfcb5e6829
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2011-10-27 20:29:32 +0400

    Formatting

--- lib/Foreign/BLAS/Types.hs
+++ lib/Foreign/BLAS/Types.hs
@@ -12,19 +12,19 @@ module Foreign.BLAS.Types (
     BLASTrans,
     Trans(..),
     withTrans,
-    
+
     BLASUplo,
     Uplo(..),
     withUplo,
-    
+
     BLASDiag,
     Diag(..),
     withDiag,
-    
+
     BLASSide,
     Side(..),
     withSide,
-    
+
     LAInt,
     ) where
 
@@ -33,7 +33,11 @@ import Foreign.C.Types
 import Foreign.C.String
 
 newtype BLASTrans = BLASTrans CString
-data Trans = NoTrans | Trans | ConjTrans deriving (Eq, Show)
+
+data Trans = NoTrans
+           | Trans
+           | ConjTrans
+           deriving (Eq, Show)
 
 withTrans :: Trans -> (BLASTrans -> IO a) -> IO a
 withTrans trans f = flip withCString (f . BLASTrans) $ case trans of
@@ -42,7 +46,10 @@ withTrans trans f = flip withCString (f . BLASTrans) $ case trans of
     ConjTrans -> "C"
 
 newtype BLASUplo = BLASUplo CString
-data Uplo = Upper | Lower deriving (Eq, Show)
+
+data Uplo = Upper
+          | Lower
+          deriving (Eq, Show)
 
 withUplo :: Uplo -> (BLASUplo -> IO a) -> IO a
 withUplo uplo f = flip withCString (f . BLASUplo) $ case uplo of
@@ -50,7 +57,10 @@ withUplo uplo f = flip withCString (f . BLASUplo) $ case uplo of
     Lower -> "L"
 
 newtype BLASSide = BLASSide CString
-data Side = LeftSide | RightSide deriving (Eq, Show)
+
+data Side = LeftSide
+          | RightSide
+          deriving (Eq, Show)
 
 withSide :: Side -> (BLASSide -> IO a) -> IO a
 withSide side f = flip withCString (f . BLASSide) $ case side of
@@ -58,11 +68,15 @@ withSide side f = flip withCString (f . BLASSide) $ case side of
     RightSide -> "R"
 
 newtype BLASDiag = BLASDiag CString
-data Diag = NonUnit | Unit deriving (Eq, Show)
+
+data Diag = NonUnit
+          | Unit
+          deriving (Eq, Show)
 
 withDiag :: Diag -> (BLASDiag -> IO a) -> IO a
 withDiag diag f = flip withCString (f . BLASDiag) $ case diag of
     NonUnit -> "N"
     Unit    -> "U"
+
 
 newtype LAInt = LAInt CInt deriving (Eq, Show, Enum, Num, Ord, Storable)
