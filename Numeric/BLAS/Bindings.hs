e0106d030ca389043562efda13c6abb68228659d
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2012-08-10 17:45:17 +0400

    Update documentation on memory layout

--- Numeric/BLAS/Bindings.hs
+++ Numeric/BLAS/Bindings.hs
@@ -34,17 +34,17 @@ import Numeric.BLAS.Bindings.Types
 
 -- $memory
 --
--- Here memory layout of vectors and matrices is described. Note that
--- I'm not an BLAS expert and this doucmentation may contain errors.
+-- Here memory layout of vectors and matrices is described.
 --
 --
 -- [@Vectors@]
 --
--- Vectors in the BLAS are strided which means elements are not laid
--- contigously (stride 1) in memory but separated by some number of
--- other elements. This way vectors could represent both rows and
--- columns of matrix among other things. For example here is vector
--- with stride 3:
+-- Vectors in the BLAS are strided which means elements are not
+-- nessesarily laid contigously (stride 1) in memory but separated by
+-- some number of other elements. This way vectors could represent
+-- both rows and columns of matrix among other things. For example
+-- here is vector with stride 3. Elements which are not refernced
+-- denoted with @_@.
 --
 -- > 1 _ _ 2 _ _ 3 ...
 --
@@ -52,19 +52,19 @@ import Numeric.BLAS.Bindings.Types
 -- [@Matrices@]
 --
 -- Matrix layout is more complicated and there are many variants of
--- matrices. First question is whether data is column major or row
--- major. By default FORTRAN (and so BLAS) uses column major and C row
--- major ordering. CBLAS can use both which is chosen by first
--- parameter in functions where it matters ('RowOrder'). Everywhere
--- column major order is assumed.
+-- matrices. First option is whether data is stored in column major or
+-- row major order. By default FORTRAN (and so BLAS) uses column major
+-- and C row major ordering. CBLAS can use both which is chosen by
+-- first parameter in functions where it matters
+-- ('RowOrder'). Everywhere column major order is assumed.
 --
 --
 -- [@Generic dense matrices@]
 --
 -- It's simplest matrix and it's laid in memory like that:
 --
--- > 1 4 6   ↑
--- > 2 5 7   │ LDA
+-- > 1 4 6   ↑         / 1 4 6 \
+-- > 2 5 7   │ LDA   ⇒ \ 2 5 7 /
 -- > _ _ _   ↓
 --
 -- This matrix have 2 rows and 3 columns. Similarly to vectors columns
@@ -73,11 +73,17 @@ import Numeric.BLAS.Bindings.Types
 -- existing matrix.
 --
 --
--- [@Dense symmetric/hermitian matrices@]
+-- [@Dense symmetric\/hermitian\/triangular matrices@]
 --
--- Packed symmetric/hermitian matrices have two variants of storage.
+-- These matrices are laid in memory just like general matrices only
+-- part of the data is actually used. Whether part above or below
+-- diagonal is used determined by 'Uplo' parameter. Here is example
+-- for symmetric matrix:
 --
--- > Upper      Lower
--- > 1 2 4      1 4 6
--- >   3 5      2 5
--- >     6      3
+-- > 1 4 7      1 4 7
+-- > _ 5 8   ⇒  4 5 8      Upper
+-- > _ _ 9      7 8 9
+--
+-- > 1 _ _      1 2 3
+-- > 2 5 _   ⇒  2 5 6      Lower
+-- > 3 6 9      3 6 9
